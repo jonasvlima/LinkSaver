733c5c0753c63a451f7892acf709b171361d3852
Jonas Lima <133159361+jonasvlima@users.noreply.github.com>
2025-02-21 23:56:34 -0300

    Update input components styling and behavior

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
@@ -8,16 +9,16 @@ import { Link } from "@/components/link"
 import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
 
-export default function Index () {
-    return(
-        <View style={styles.container}>
-            <View style={styles.header}>
-                    <Image source={require("@/assets/logo.png")} style={styles.logo}></Image>
-
-                    <TouchableOpacity><MaterialIcons name="add" size={32} 
-                    color={colors.green[300]}></MaterialIcons>
-                    </TouchableOpacity>
-            </View>
+export default function Index() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={require("@/assets/logo.png")} style={styles.logo} />
+  
+          <TouchableOpacity onPress={() => router.navigate("/add")}>
+            <MaterialIcons name="add" size={32} color={colors.green[300]} />
+          </TouchableOpacity>
+        </View>
 
             <Categories />
 
@@ -34,7 +35,7 @@ export default function Index () {
                 contentContainerStyle={styles.linksContent}
                 showsVerticalScrollIndicator={false}
             />
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
